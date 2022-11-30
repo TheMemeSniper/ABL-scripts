19de8b81cc2807ea09927759e170c8ddef3ae2df
TheMemeSniper <67394719+TheMemeSniper@users.noreply.github.com>
2022-11-30 09:26:40 -0600

    Add files via upload

---
+++ repoindex.py
@@ -0,0 +1,8 @@
+repoindex = {
+    "linux-stable": ["fork-bomb.py","aleks6.py"],
+    "linux-unstable": ["THE ONE PIECE.py","THE HALF PIECE.py"],
+    "win-stable": ["fork-bomb-win.py","athena.py"],
+    "win-unstable": ["rainvoker.bat"],
+    "multplat-stable": ["transprideterminal.py"],
+    "multplat-unstable": ["form-cracker.py"]
+}
